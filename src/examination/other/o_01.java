32e3c4c076466a93fe71b4ea9cb86aea7f2d25ee
zxl <1207346575@qq.com>
2019-06-20 10:42:07 +0800

    leetcode

---
+++ src/examination/other/o_01.java
@@ -0,0 +1,15 @@
+package examination.other;
+
+/**
+ * 外部排序
+ * 如何排序10G个元素（PriorityQueue）
+ * Created by ZXL on 2019/6/20.
+ *
+ * 归并：使用Iterable<T>接口
+ *      1）可以不断获取下一个元素的能力
+ *      2）元素存储/获取方式被抽象，与归并结点无关
+ *      3）Iterable<T> merge(List<Iterable<T>> sortedData);
+ */
+public class o_01 {
+
+}
