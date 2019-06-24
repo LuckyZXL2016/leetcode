a77ca9f7dae890aad5b26dec24131410daa9cdf1
zxl <1207346575@qq.com>
2019-06-24 09:57:16 +0800

    leetcode

---
+++ src/examination/google/g_01.java
@@ -0,0 +1,42 @@
+package examination.google;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * beautifulNumber
+ * 判断一个数在几进制下是beautiful的
+ * Created by ZXL on 2019/6/24.
+ */
+public class g_01 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(
+                new BufferedReader(new InputStreamReader(System.in)));
+        int cases = in.nextInt();
+        for (int i = 0; i < cases; i++) {
+            int n = in.nextInt();
+            System.out.println("Case #" + i + ":" + beautiful(n));
+        }
+    }
+
+    private static int beautiful(int n) {
+        for (int radix = 2; radix < n; radix++) {
+            // if n is 11 in radix, return radix
+            if (isBeautiful(n, radix)) {
+                return radix;
+            }
+        }
+        return n-1;
+    }
+
+    private static boolean isBeautiful(int n, int radix) {
+        while (n > 0) {
+            if (n % radix != 1) {
+                return false;
+            }
+            n /= radix;
+        }
+        return true;
+    }
+}
