1338c1da3ad48d8dfce07b1d23101ffe5db95521
zxl <1207346575@qq.com>
2019-07-12 14:36:59 +0800

    leetcode

--- src/leetCode/array/array07.java
+++ src/leetCode/array/array07.java
@@ -7,20 +7,20 @@ import java.util.List;
  * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
  * 说明：解集不能包含重复的子集。
  * Created by ZXL on 2019/7/12.
- *
+ * <p>
  * 示例:
- *  输入: nums = [1,2,3]
- *  输出:
- *  [
- *      [3],
- *      [1],
- *      [2],
- *      [1,2,3],
- *      [1,3],
- *      [2,3],
- *      [1,2],
- *      []
- *  ]
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ * [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
  */
 public class array07 {
 
@@ -32,7 +32,7 @@ public class array07 {
         for (int i = 0; i < len; i++) {
             List<List<Integer>> ans_temp = new ArrayList<>();
 
-            for (List<Integer> list:ans) {
+            for (List<Integer> list : ans) {
                 List<Integer> ls = new ArrayList<>(list);
                 ls.add(nums[i]);
                 ans_temp.add(ls);
