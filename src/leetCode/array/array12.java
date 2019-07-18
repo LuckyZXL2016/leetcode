580959c7ecaa2f0c1af36acd1a4356c7a4d6d260
zxl <1207346575@qq.com>
2019-07-18 09:44:00 +0800

    leetcode

--- src/leetCode/array/array12.java
+++ src/leetCode/array/array12.java
@@ -1,6 +1,7 @@
 package leetCode.array;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 /**
  * 在未排序的数组中找到第 k个最大的元素。请注意，你需要找的是数组排序
@@ -13,10 +14,24 @@ import java.util.Arrays;
  */
 public class array12 {
 
-
-    public int findKthLargest(int[] nums, int k) {
+    // 方法一：排序
+    public int findKthLargest1(int[] nums, int k) {
         int len = nums.length;
         Arrays.sort(nums);
         return nums[len - k - 1];
+    }
+
+    // 方法二：堆
+    // 创建一个小顶堆，用PriorityQueue实现最大最小堆：https://www.cnblogs.com/yongh/p/9945539.html
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> n1 - n2);
+
+        for (int num: nums) {
+            heap.add(num);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+        return heap.poll();
     }
 }
