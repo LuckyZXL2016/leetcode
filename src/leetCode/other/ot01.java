cd522583dc8f239499472e865fa29aed912ad6de
zxl <1207346575@qq.com>
2019-07-19 16:48:10 +0800

    leetcode

--- src/leetCode/other/ot01.java
+++ src/leetCode/other/ot01.java
@@ -3,18 +3,20 @@ package leetCode.other;
 /**
  * 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
  * Created by ZXL on 2019/7/19.
- *
+ * <p>
  * 示例 :
- *  输入: 1
- *  输出: true
- *  解释: 2^0 = 1
+ * 输入: 1
+ * 输出: true
+ * 解释: 2^0 = 1
  */
 public class ot01 {
-    public boolean isPowerOfTwo(int n) {
+
+    // 方法一：循环
+    public boolean isPowerOfTwo1(int n) {
         if (n <= 0) return false;
         if (n == 1) return true;
 
-        while ( n != 1) {
+        while (n != 1) {
             int tmp = n / 2;
             if (tmp * 2 != n) {
                 return false;
@@ -22,5 +24,10 @@ public class ot01 {
             n = n / 2;
         }
         return true;
+    }
+
+    // 方法二：二进制
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
