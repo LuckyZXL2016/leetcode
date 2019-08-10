e1086fa21e7d9941759eb1a69496751035ccbbe9
zxl <1207346575@qq.com>
2019-08-10 15:38:38 +0800

    leetcode

--- src/leetCode/tree/tr03.java
+++ src/leetCode/tree/tr03.java
@@ -36,4 +36,18 @@ public class tr03 {
         dfs(root);
         return res;
     }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(3);
+        TreeNode t2 = new TreeNode(1);
+        TreeNode t3 = new TreeNode(4);
+        TreeNode t4 = new TreeNode(2);
+
+        t1.left = t2;
+        t1.right = t3;
+        t2.right = t4;
+
+        int val = new tr03().kthSmallest(t1, 1);
+        System.out.println(val);
+    }
 }
