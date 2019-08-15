7f39555d064a2362889035fce42568bb577ba813
zxl <1207346575@qq.com>
2019-08-15 19:45:39 +0800

    leetcode

---
+++ src/examination/pufa/p_15.java
@@ -0,0 +1,15 @@
+package examination.pufa;
+
+/**
+ * 大小写转换
+ * Created by ZXL on 2019/8/15.
+ */
+public class p_15 {
+    private static String upLowCase(String str) {
+        return str.toLowerCase();
+    }
+
+    private static String lowUpCase(String str) {
+        return str.toLowerCase();
+    }
+}
