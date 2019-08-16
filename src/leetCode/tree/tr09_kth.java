cbcc271598ad69117bd6a8f169d2c9c59070190b
zxl <1207346575@qq.com>
2019-08-16 14:20:03 +0800

    leetcode

--- src/leetCode/tree/tr09_kth.java
+++ src/leetCode/tree/tr09_kth.java
@@ -6,20 +6,37 @@ package leetCode.tree;
  */
 public class tr09_kth {
     private int count = 0;
-    private int val = 0;
+    private int res = 0;
 
-    private int kthNode(TreeNode root, int k) {
-        count = k;
-        dfs(root);
-        return val;
-    }
-
-    private void dfs(TreeNode root) {
-        if (root == null) {
+    private void dfs(TreeNode node) {
+        if (node == null) {
             return;
         }
-        dfs(root.left);
+        dfs(node.right);
         count--;
+        if (count == 0) {
+            this.res = node.val;
+        }
+        dfs(node.left);
+    }
 
+    public int kthBiggest(TreeNode root, int k) {
+        count = k;
+        dfs(root);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(3);
+        TreeNode t2 = new TreeNode(1);
+        TreeNode t3 = new TreeNode(4);
+        TreeNode t4 = new TreeNode(2);
+
+        t1.left = t2;
+        t1.right = t3;
+        t2.right = t4;
+
+        int val = new tr09_kth().kthBiggest(t1, 1);
+        System.out.println(val);
     }
 }
