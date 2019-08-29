b5eb52c3c92f9d921b99bc3b48feda8c4fc628d4
zxl <1207346575@qq.com>
2019-08-29 13:03:06 +0800

    leetcode

---
+++ src/studyForJob/s_05.java
@@ -0,0 +1,40 @@
+package studyForJob;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * 实现括号是否匹配
+ * Created by ZXL on 2019/8/29.
+ */
+public class s_05 {
+    private static boolean isMatch(String str) {
+        if (str == null || str.length() == 0) return false;
+
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            char c = str.charAt(i);
+            if (map.containsValue(c)) {
+                stack.push(c);
+            } else if (map.containsKey(c)) {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    if (stack.peek() == c) {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
