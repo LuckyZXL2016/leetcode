6d81746aa3fb8b5bee2b7327d3bf71c7c526b829
zxl <1207346575@qq.com>
2019-08-30 18:45:53 +0800

    leetcode

--- src/studyForJob/s_18.java
+++ src/studyForJob/s_18.java
@@ -31,18 +31,12 @@ public class s_18 {
 
         int len1 = str1.length();
         int len2 = str2.length();
-        int[][] dp = new int[len1][len2];
-        for (int i = 0; i < len2; i++) {
-            dp[0][i] = (str1.charAt(0) == str2.charAt(i)) ? 1 : 0;
-        }
-        for (int i = 0; i < len1; i++) {
-            dp[i][0] = (str1.charAt(i) == str2.charAt(0)) ? 1 : 0;
-        }
+        int[][] dp = new int[len1 + 1][len2 + 1];
 
-        int maxLen = 0, maxEnd = 0;
-        for (int i = 1; i < len1; i++) {
-            for (int j = 1; j < len2; j++) {
-                if (str1.charAt(i) == str2.charAt(j)) {
+        int maxLen = 1, maxEnd = 0;
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                     if (maxLen < dp[i][j]) {
                         maxLen = dp[i][j];
@@ -53,8 +47,8 @@ public class s_18 {
                 }
             }
         }
-        int start = maxEnd - maxLen + 1;
-        return str1.substring(start, maxEnd + 1);
+        int start = maxEnd - maxLen;
+        return str1.substring(start, maxEnd);
     }
 
     public static void main(String[] args) {
