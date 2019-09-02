face62165f73f59792c884fc952e0208440cfb19
zxl <1207346575@qq.com>
2019-09-02 19:49:42 +0800

    leetcode

--- src/leetCode/dp/dp09.java
+++ src/leetCode/dp/dp09.java
@@ -1,5 +1,6 @@
 package leetCode.dp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,7 +18,7 @@ import java.util.List;
  * 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
  */
 public class dp09 {
-    public int minimumTotal(List<List<Integer>> triangle) {
+    private static int minimumTotal(List<List<Integer>> triangle) {
         int[] dp = new int[triangle.size() + 1];
         for (int i = triangle.size() - 1; i >= 0; i--) {
             List<Integer> curTr = triangle.get(i);
@@ -28,14 +29,41 @@ public class dp09 {
         return dp[0];
     }
 
-    public int minimumTotal2(List<List<Integer>> triangle) {
-        int[][] dp = new int[triangle.size() + 1][triangle.size() + 1];
-        for (int i = triangle.size() - 1; i >= 0; i--) {
-            List<Integer> curTr = triangle.get(i);
-            for (int j = 0; j < curTr.size(); j++) {
-                dp[i][j] = Math.min(dp[i+1][j], dp[i+1][j+1]) + curTr.get(j);
+    private static int minimumTotal2(List<List<Integer>> triangle) {
+        int len = triangle.size();
+        // dp[i][j]为 自底向上第 i层j列最小和
+        int[][] dp = new int[len + 1][len + 1];
+
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
             }
         }
         return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(2);
+        List<Integer> l2 = new ArrayList<>();
+        l2.add(3);
+        l2.add(4);
+        List<Integer> l3 = new ArrayList<>();
+        l3.add(6);
+        l3.add(5);
+        l3.add(7);
+        List<Integer> l4 = new ArrayList<>();
+        l4.add(4);
+        l4.add(1);
+        l4.add(8);
+        l4.add(3);
+        triangle.add(l1);
+        triangle.add(l2);
+        triangle.add(l3);
+        triangle.add(l4);
+
+        System.out.println(minimumTotal(triangle));
+        System.out.println(minimumTotal2(triangle));
     }
 }
