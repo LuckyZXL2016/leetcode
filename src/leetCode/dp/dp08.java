0dab79c24dbabc500bb153be26b6b0c40d25b888
zxl <1207346575@qq.com>
2019-09-02 15:58:15 +0800

    leetcode

--- src/leetCode/dp/dp08.java
+++ src/leetCode/dp/dp08.java
@@ -1,5 +1,8 @@
 package leetCode.dp;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
  * Created by ZXL on 2019/3/11.
@@ -7,8 +10,18 @@ package leetCode.dp;
  * 输入: 3
  * 输出: 5
  */
+class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int val;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
 public class dp08 {
-    public int numTrees(int n) {
+    private static int numTrees(int n) {
         int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = 1;
@@ -20,8 +33,31 @@ public class dp08 {
         return dp[n];
     }
 
+    private static List<TreeNode> getSubTrees(int start, int end) {
+        List<TreeNode> res = new LinkedList<>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> subLeftTrees = getSubTrees(start, i - 1);
+            List<TreeNode> subRightTrees = getSubTrees(i + 1, end);
+            for (TreeNode left : subLeftTrees) {
+                for (TreeNode right : subRightTrees) {
+                    TreeNode node = new TreeNode(i);
+                    node.left = left;
+                    node.right = right;
+                    res.add(node);
+                }
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        dp08 example = new dp08();
-        System.out.println(example.numTrees(3));
+        int n = 3;
+
+        System.out.println(numTrees(n));
+        System.out.println(getSubTrees(1, n).size());
     }
 }
