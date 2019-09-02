1ab4176493d3853a32948d96f7fb74d5685d59d3
zxl <1207346575@qq.com>
2019-09-02 14:06:11 +0800

    leetcode

--- src/leetCode/dp/dp04.java
+++ src/leetCode/dp/dp04.java
@@ -5,18 +5,18 @@ package leetCode.dp;
  * 左上角到右下角的路径，使得路径上的数字总和为最小。
  * 说明：每次只能向下或者向右移动一步。
  * Created by ZXL on 2019/3/7.
- *
+ * <p>
  * 输入:
  * [
- *  [1,3,1],
- *  [1,5,1],
- *  [4,2,1]
+ * [1,3,1],
+ * [1,5,1],
+ * [4,2,1]
  * ]
  * 输出: 7
  * 解释: 因为路径 1→3→1→1→1 的总和最小。
  */
 public class dp04 {
-    public static int minPathSum(int[][] grid) {
+    private static int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         for (int i = 0; i < m; i++) {
@@ -31,15 +31,37 @@ public class dp04 {
                 }
             }
         }
-        return grid[m-1][n-1];
+        return grid[m - 1][n - 1];
+    }
+
+    private static int minPathSum2(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
-        int[][] grid = new int[][] {
-                {1,3,1},
-                {1,5,1},
-                {4,2,1}
+        int[][] grid = new int[][]{
+                {1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}
         };
+        System.out.println(minPathSum2(grid));
         System.out.println(minPathSum(grid));
     }
 }
