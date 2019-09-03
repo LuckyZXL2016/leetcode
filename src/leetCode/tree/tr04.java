23284195c4a3a4a5692af5fc0e84ac72beac4e36
zxl <1207346575@qq.com>
2019-09-03 15:45:38 +0800

    leetcode

--- src/leetCode/tree/tr04.java
+++ src/leetCode/tree/tr04.java
@@ -29,26 +29,26 @@ public class tr04 {
     }
 
     public static void main(String[] args) {
-        TreeNode t1 = new TreeNode(6);
-        TreeNode t2 = new TreeNode(2);
-        TreeNode t3 = new TreeNode(8);
-        TreeNode t4 = new TreeNode(0);
-        TreeNode t5 = new TreeNode(4);
-        TreeNode t6 = new TreeNode(7);
-        TreeNode t7 = new TreeNode(9);
-        TreeNode t8 = new TreeNode(3);
-        TreeNode t9 = new TreeNode(5);
+        TreeNode t1 = new TreeNode(3);
+        TreeNode t2 = new TreeNode(5);
+        TreeNode t3 = new TreeNode(1);
+        TreeNode t4 = new TreeNode(6);
+        TreeNode t5 = new TreeNode(2);
+        TreeNode t6 = new TreeNode(0);
+        TreeNode t7 = new TreeNode(8);
+        TreeNode t8 = new TreeNode(7);
+        TreeNode t9 = new TreeNode(4);
         t1.left = t2;
         t1.right = t3;
         t2.left = t4;
         t2.right = t5;
         t3.left = t6;
         t3.right = t7;
-        t6.left = t8;
-        t6.right = t9;
+        t5.left = t8;
+        t5.right = t9;
 
-        TreeNode p = new TreeNode(2);
-        TreeNode q = new TreeNode(8);
+        TreeNode p = new TreeNode(5);
+        TreeNode q = new TreeNode(1);
 
         TreeNode node = lowestCommonAncestor(t1, p, q);
         System.out.println(node.val);
